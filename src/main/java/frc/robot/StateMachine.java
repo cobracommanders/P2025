42782486aeac7d601e91c7ba5b42400ffaade5f2
Steph3n-T <stamasi@498robotics.org>
2025-01-17 16:35:33 -0700

    added doglog

--- src/main/java/frc/robot/StateMachine.java
+++ src/main/java/frc/robot/StateMachine.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import dev.doglog.DogLog;
 
 import java.util.Set;
 
