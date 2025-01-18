d0d5892f3a8da9eb1ec6f15852249af66c661854
Bella <139650496+Cosm0tron@users.noreply.github.com>
2025-01-18 14:42:04 -0800

    Added and wrote manipulator state, positions, and subsystem
    1/18/2025
    3:41pm

---
+++ src/main/java/frc/robot/subsystems/manipulator/ManipulatorPositions.java
@@ -0,0 +1,10 @@
+package frc.robot.subsystems.manipulator;
+
+public class ManipulatorPositions {
+    public static final double IDLE = 0;
+    public static final double INTAKE_CORAL = 0;
+    public static final double L1 = 0;
+    public static final double L2 = 0;
+    public static final double L3 = 0;
+    public static final double L4 = 0;
+}
---
+++ src/main/java/frc/robot/subsystems/manipulator/ManipulatorSubsystem.java
@@ -0,0 +1,102 @@
+package frc.robot.subsystems.manipulator;
+
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.units.measure.Angle;
+import frc.robot.Constants.ManipulatorConstants;
+import frc.robot.Ports;
+import frc.robot.StateMachine;
+
+
+
+public class ManipulatorSubsystem extends StateMachine<ManipulatorState>{
+    private final TalonFX manipulatorMotor;
+    private final TalonFXConfiguration motor_config = new TalonFXConfiguration();
+    private double manipulatorPosition;
+  
+    private PositionVoltage motor_request = new PositionVoltage(0).withSlot(0);
+    
+    private ManipulatorState currentState;
+    private double setpoint;
+    private double manualSpeed;
+    
+    private boolean isActivated = true;
+    
+    public ManipulatorSubsystem() {
+      super(ManipulatorState.IDLE);
+      manipulatorMotor = new TalonFX(Ports.ManipulatorPorts.MANIPULATOR_MOTOR);
+      currentState = ManipulatorState.IDLE;
+    }
+  
+     public boolean atGoal() {
+      return switch (getState()) {
+        case IDLE -> 
+          MathUtil.isNear(ManipulatorPositions.IDLE, manipulatorPosition, 0.1);
+          case INTAKE_CORAL -> 
+          MathUtil.isNear(ManipulatorPositions.IDLE, manipulatorPosition, 0.1);
+        case L1 ->
+          MathUtil.isNear(ManipulatorPositions.L1, manipulatorPosition, 0.1);
+        case L2 ->
+          MathUtil.isNear(ManipulatorPositions.L2, manipulatorPosition, 0.1);
+        case L3 ->
+          MathUtil.isNear(ManipulatorPositions.L3, manipulatorPosition, 0.1);
+        case L4 ->
+          MathUtil.isNear(ManipulatorPositions.L3, manipulatorPosition, 0.1);
+      };
+    }
+  
+    public void setState(ManipulatorState newState) {
+        setStateFromRequest(newState);
+      }
+  
+    public void updatePosition(){
+      manipulatorPosition = manipulatorMotor.getPosition().getValueAsDouble();
+    }
+  
+    public void setManipulatorPositions(double manipulatorPosition){
+      manipulatorMotor.setControl(motor_request.withPosition(manipulatorPosition));
+    }
+  
+      @Override
+      protected void afterTransition(ManipulatorState newState) {
+        switch (newState) {
+          case IDLE -> {
+            setManipulatorPositions(ManipulatorPositions.IDLE);
+          }
+          case INTAKE_CORAL -> {
+            setManipulatorPositions(ManipulatorPositions.INTAKE_CORAL);
+          }
+          case L1 -> {
+            setManipulatorPositions(ManipulatorPositions.L1);
+          }
+          case L2 -> {
+            setManipulatorPositions(ManipulatorPositions.L2);
+          }
+          case L3 -> {
+            setManipulatorPositions(ManipulatorPositions.L3);
+          }
+          case L4 -> {
+            setManipulatorPositions(ManipulatorPositions.L4);
+          }
+          default -> {}
+        }
+      }
+  
+    @Override
+    public void periodic() {
+      updatePosition();
+    }
+    private static ManipulatorSubsystem instance;
+  
+    public static ManipulatorSubsystem getInstance() {
+        if (instance == null) instance = new ManipulatorSubsystem(); // Make sure there is an instance (this will only run once)
+        return instance;
+    }
+  }
